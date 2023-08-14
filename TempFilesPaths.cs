95c05d12742f1592903ae4216c1d7ad57962f966
Daniil Demyanenko <daniil-demyanenko1@ya.ru>
2023-08-14 21:16:33 +0300

    Класс для получения путей для новых файлов.

---
+++ TempFilesPaths.cs
@@ -0,0 +1,15 @@
+namespace DictCombine;
+
+public static class TempFilesPaths
+{
+    public static string TempDirPath => _tempDirPath;
+
+    private static string _tempDirPath = Path.GetTempPath() + String.Join("", Guid.NewGuid().ToString().Take(8)) + "/";
+
+    public static string Next()
+    {
+        if (!Directory.Exists(_tempDirPath)) Directory.CreateDirectory(_tempDirPath);
+
+        return _tempDirPath + Guid.NewGuid();
+    }
+}

719a175c2c4f6de8caa3d5e7fdc558cde2d8cbb0
Daniil Demyanenko <daniil-demyanenko1@ya.ru>
2023-08-17 23:18:23 +0300

    BigFileSorter теперь не объединяет отсортированные части в один файл

--- BigFileSorter.cs
+++ BigFileSorter.cs
@@ -13,42 +13,48 @@ public class BigFileSorter
     /// <summary>
     /// Сортирует строки в указаном файле.
     /// </summary>
-    /// <returns>Путь к отсортированному файлу.</returns>
-    public async Task<string> Sort()
+    /// <returns>Путь к отсортированным файлам, кол-во паролей в файле.</returns>
+    public async Task<(List<string> Path, long Count)> Sort()
     {
-        var partsPaths = await SplitFile();
-        var result = await SortAndMargeFiles(partsPaths);
-        
+        var (partsPaths, count) = await SplitFile();
+        //var path = await SortAndMargeFiles(partsPaths);
+
         foreach (var f in partsPaths)
             if (File.Exists(f))
                 File.Delete(f);
-        
-        return result;
+
+        return (partsPaths, count);
     }
-    
+
     /// <summary>
     /// Разбивает файл на куски, которые могут поместиться в памяти и сортирует их.
     /// </summary>
-    /// <returns>Пути к отсортированным файлам.</returns>
-    private async Task<List<string>> SplitFile()
+    /// <returns>Пути к отсортированным файлам, кол-во паролей в файле.</returns>
+    private async Task<(List<string> partsPaths, long Count)> SplitFile()
     {
         List<string> partsPaths = new();
         List<Task> writeTasks = new();
-        var input = File.ReadLines(_inputFilePath); 
-        
+        long count = 0;
+        var input = File.ReadLines(_inputFilePath);
+
         foreach (var i in input.Chunk(_bufferSize))
         {
-            var partPath = TempFilesPaths.Next(); 
+            var partPath = TempFilesPaths.Next();
             partsPaths.Add(partPath);
-            
+
             // Сортируем во время нарезки, чтоб сэкономить на чтении/записи файлов
-            var sorted = i.OrderBy(x => x);
-            writeTasks.Add(File.WriteAllLinesAsync(partPath, sorted)); 
+            var sorted = i.OrderBy(x => x).Select(x =>
+            {
+                ++count;
+                return x;
+            });
+
+            writeTasks.Add(File.WriteAllLinesAsync(partPath, sorted));
         }
-        
+
         await Task.WhenAll(writeTasks);
-        
-        return partsPaths;
+
+        return (partsPaths, count);
     }
 
     /// <summary>
@@ -63,7 +69,7 @@ public class BigFileSorter
 
         try
         {
-            var lines = readers.Select( x => new FileSortItem(x)).ToList();
+            var lines = readers.Select(x => new FileSortItem(x)).ToList();
 
             await using var writer = new StreamWriter(resultPath);
 
@@ -72,7 +78,7 @@ public class BigFileSorter
                 var current = lines.OrderBy(x => x.Line).First();
                 await writer.WriteLineAsync(current.Line);
 
-                if (! await current.TryNextAsync()) lines.Remove(current);
+                if (!await current.TryNextAsync()) lines.Remove(current);
             }
 
             writer.Close();
@@ -84,28 +90,6 @@ public class BigFileSorter
         }
 
         return resultPath;
-    }
-
-    private async IAsyncEnumerable<IAsyncEnumerable<string>> ChunkAsync(IAsyncEnumerable<string> input, int count)
-    {
-        await using var enumerator = input.GetAsyncEnumerator();
-        var result = new List<string>(count);
-
-        int i = 0;
-        while (await enumerator.MoveNextAsync())
-        {
-            result.Add(enumerator.Current);
-            i++;
-
-            if (i >= count)
-            {
-                i = 0;
-                yield return result.ToAsyncEnumerable();
-                result.Clear();
-            }
-        }
-
-        if (result.Count != 0) yield return result.ToAsyncEnumerable();
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+using System.Linq;
 
 namespace DictCombine;
 
 static class Program
 {
-    private const int _bufferSize = 100_000;
+    private const int _bufferSize = 1_000_000; //150_000_000;
     private static Dictionary<string, int> _passwords = new Dictionary<string, int>();
 
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         string[] inputFilesPaths;
         int bufferSize;
@@ -25,6 +26,14 @@ static class Program
             return;
         }
 
-        using Aggregator aggregator = new(bufferSize, inputFilesPaths, args[1]);
+        Console.WriteLine($"Temp folder: {TempFilesPaths.TempDirPath}");
+
+       // using Aggregator aggregator = new(bufferSize, inputFilesPaths, args[1]);
+       // var duplicates = await aggregator.Aggregate();
+        // foreach (var i in duplicates.OrderBy(x => x.Value))
+        //     Console.WriteLine($"{i.Value} {i.Key}");
+        Console.WriteLine(await new BigFileSorter(inputFilesPaths[1], bufferSize).Sort());
+        Console.ReadLine();
+        Directory.Delete(TempFilesPaths.TempDirPath, recursive: true);
     }
 }
